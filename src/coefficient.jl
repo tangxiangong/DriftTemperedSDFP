8b5b6fe6e4d88e9affb6652b0079094036c791a9
tangxiangong <tangxiangong@gmail.com>
2026-06-26 14:53:13 +0800

    Format coefficient and mesh modules

--- src/coefficient.jl
+++ src/coefficient.jl
@@ -1,11 +1,11 @@
 module coefficient
 export coe
 function coe(α::Float64, μ::Float64, τ::Float64, n::Int64)
-    c = 1/(1+τ*μ)
-    ω = zeros(n+1)
-    @inbounds ω[1] = ((1+τ*μ)^α - (τ*μ)^α) / τ^α
+    c = 1 / (1 + τ * μ)
+    ω = zeros(n + 1)
+    @inbounds ω[1] = ((1 + τ * μ)^α - (τ * μ)^α) / τ^α
     @inbounds for k in 1:n
-        ω[k+1] = c * (1-(α+1)/k) * ω[k]
+        ω[k+1] = c * (1 - (α + 1) / k) * ω[k]
     end
     return ω
 end
